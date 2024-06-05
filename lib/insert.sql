91827352144472ad9e597bf9424f8ef1134617e2
Stephen Ochieng <materiamystic@gmail.com>
2024-06-05 17:22:24 +0300

    Adds required sql queries and statements

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 7, "M", "black","mild", 1),
+("Rowdy", 8, "M", "black","fierce", 1), 
+("Tabitha", 7, "F", "black","cool", 1), 
+("Sergeant Brown", 10, "M", "brown", "fierce", 0), 
+("Melissa", 9, "F", "black","fierce", 0),
+("Grinch", 7, "M", "black","fierce", 1), 
+("Wendy", 9, "F", "brown","cool", 1), 
+(NULL, 2, "M", "white", "adorable", 1);
